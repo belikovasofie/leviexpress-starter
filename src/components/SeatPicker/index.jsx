1889852f67f60049c2fff3cabb2c8e9105f7d070
Sofya Belikova <belikovasofie@gmail.com>
2021-05-29 18:42:39 +0200

    potvrzeni rezevace

--- src/components/SeatPicker/index.jsx
+++ src/components/SeatPicker/index.jsx
@@ -1,9 +1,29 @@
 import React, { useState } from 'react';
-import './style.css';
+import { useHistory } from 'react-router';
 import { SeatRow } from '../SeatRow';
+import './style.css';
 
 export const SeatPicker = ({ seats, journeyId }) => {
+  const { push } = useHistory();
   const [selectedSeatNumber, setSelectedSeatNumber] = useState(null);
+
+  const handleBuy = () => {
+    fetch('https://leviexpress-backend.herokuapp.com/api/reserve', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        seat: selectedSeatNumber,
+        journeyId,
+      }),
+    })
+      .then((resp) => resp.json())
+      .then((json) => {
+        const reservationId = json.data.reservationId;
+        push(`/reservation/${reservationId}`);
+      });
+  };
 
   const handleSeatSelect = (number) => {
     setSelectedSeatNumber(number);
@@ -22,7 +42,12 @@ export const SeatPicker = ({ seats, journeyId }) => {
           />
         ))}
       </div>
-      <button className="btn" type="button">
+      <button
+        disabled={selectedSeatNumber === null}
+        onClick={handleBuy}
+        className="btn"
+        type="button"
+      >
         Rezervovat
       </button>
     </div>
