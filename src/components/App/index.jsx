e4f1c86087e1dc4f565f325df456d99183798aec
Sofya Belikova <belikovasofie@gmail.com>
2021-05-29 10:36:03 +0200

    route add

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -2,11 +2,22 @@ import React from 'react';
 import { Footer } from '../Footer';
 import { Header } from '../Header';
 import { Home } from '../Home';
+import { Reservation } from '../Reservation';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
-export const App = () => (
-  <>
-    <Header />
-    <Home />
-    <Footer />
-  </>
-);
+export const App = () => {
+  return (
+    <Router>
+      <Header />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/reservation">
+          <Reservation />
+        </Route>
+      </Switch>
+      <Footer />
+    </Router>
+  );
+};
---
+++ src/components/Reservation/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const Reservation = () => {
+  return (
+    <>
+      <h2>Detail jízdenky</h2>
+    </>
+  );
+};
